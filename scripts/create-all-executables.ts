a0c0c3b7f8b1b7571a7aaf72ef076908199a331b
Shinji Yamada <dotneet@gmail.com>
2025-05-10 23:38:20 +0900

    refactor: add await to bun build commands in create-all-executables script

--- scripts/create-all-executables.ts
+++ scripts/create-all-executables.ts
@@ -3,7 +3,7 @@ import packageJson from "../package.json";
 
 const version = packageJson.version;
 
-await $`bun build --compile --outfile dist/aica-linux-x64-v${version} --target bun-linux-x64-baseline src/main.ts`;
-await $`bun build --compile --outfile dist/aica-linux-arm64-v${version} --target bun-linux-arm64-baseline src/main.ts`;
-await $`bun build --compile --outfile dist/aica-darwin-x64-v${version} --target bun-darwin-x64-baseline src/main.ts`;
-await $`bun build --compile --outfile dist/aica-darwin-arm64-v${version} --target bun-darwin-arm64-baseline src/main.ts`;
+await $`bun build --compile --outfile dist/aica-v${version}-linux-x64 --target bun-linux-x64-baseline src/main.ts`;
+await $`bun build --compile --outfile dist/aica-v${version}-linux-arm64 --target bun-linux-arm64-baseline src/main.ts`;
+await $`bun build --compile --outfile dist/aica-v${version}-darwin-x64 --target bun-darwin-x64-baseline src/main.ts`;
+await $`bun build --compile --outfile dist/aica-v${version}-darwin-arm64 --target bun-darwin-arm64-baseline src/main.ts`;
